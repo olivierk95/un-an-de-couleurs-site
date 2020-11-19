5e139113754e622e9ac5564ec25004721b335d82
[olivier-keutgens] <[keutgensolivier@gmail.com]>
2020-11-19 12:42:31 +0100

    google analytics new id

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-CQNTVZ24FY",
+        trackingId: "253371693",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
